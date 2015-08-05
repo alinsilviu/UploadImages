87af16db9a1c8a6e23fe8af64e9161959c2d49c3
Alin Palici <silviualin067@gmail.com>
2015-08-05 19:49:57 +0300

    Upload Images

---
+++ show_image.php
@@ -0,0 +1,23 @@
+<?php
+$name = FALSE;
+if (isset($_GET['image'])) {
+	$ext = strtolower (substr($_GET['image'], -4));
+	if (($ext == '.jpg') OR ($ext == 'jpeg') OR ($ext == '.png')) {
+		$image = "/var/www/html/uploadphp/uploads/{$_GET['image']}";
+		if (file_exists($image) && (is_file($image))) {
+			$name = $_GET['image'];
+		}
+	}
+}
+if (!$name) {
+	$image = 'images/unavailable.png';
+	$name = 'unavailable.png';
+}
+$info = getimagesize($image);
+$fs = filesize($image);
+
+header("Content-Type: {$info['mime']}\n");
+header("Content-Disposition: inline; filename=\"$name\"\n");
+header("Content-Length: $fs\n");
+
+readfile ($image);
